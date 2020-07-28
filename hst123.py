21d84745406ddf9c1ba6bf23f8a4eb5628433b60
charliekilpatrick <ckilpatrick@gmail.com>
2020-07-28 00:35:46 -0700

    Added AttributeError to data validation check

--- hst123.py
+++ hst123.py
@@ -1569,7 +1569,7 @@ class hst123(object):
         for h in hdu:
             if h.data is not None and h.name.upper()=='SCI':
                 check = True
-    except (OSError, TypeError):
+    except (OSError, TypeError, AttributeError):
         warning = 'WARNING: {img} is empty or corrupted.  '
         warning += 'Trying to download again...'
         print(warning.format(img=image))
@@ -1601,7 +1601,7 @@ class hst123(object):
                 for h in hdu:
                     if h.data is not None and h.name.upper()=='SCI':
                         check = True
-            except (OSError, TypeError):
+            except (OSError, TypeError, AttributeError):
                 warning = 'WARNING: could not find or download {img}'
                 return(warning.format(img=image), False)
 
